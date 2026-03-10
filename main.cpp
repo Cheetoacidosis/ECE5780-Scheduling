85689ab3756f265dc48a5238db852f23d0b34c0a
CallieK <callie.kubesh@usu.edu>
2026-03-10 12:35:20 -0600

    Created makefile & main.cpp

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+//#include "RateMonotonic.h"/
+
+int main() {
+    printf("What's up gaymers. It's your girl, liver cirrosis\n");
+
+    return 0;
+}
---
+++ makefile
@@ -0,0 +1,2 @@
+scheduler: main.cpp
+	g++ main.cpp --output scheduler
